7ac5ba8b5abe1ab9c0b77e96351cfea9516089c1
Pratibha-Sharma1507 <sharmamunnu555@gmail.com>
2026-03-16 12:50:49 +0530

    vercel deploy1

--- api/send-email.js
+++ api/send-email.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       service: "gmail",
       auth:{
         user: "sharmapratibha1507@gmail.com",
-        pass: "APP_PASSWORD"
+        pass: "ltpq zbwq qvfj yfyx"
       }
     });
 
